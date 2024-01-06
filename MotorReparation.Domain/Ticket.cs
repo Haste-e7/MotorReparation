17aaec5b9feb5246bcc32acbc7d5c3c5e1173c76
thedong123 <tdong899@gmail.com>
2024-01-06 18:13:25 +0700

    add entities and string definition

---
+++ MotorReparation.Domain/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace MotorReparation.Domain
+{
+    public class Ticket: EntityBase
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+        public string? TicketType { get; set; }
+        public int AssignedBay { get; set; }
+    }
+}
---
+++ MotorReparation.Infrastructure/StringDefinition.cs
@@ -0,0 +1,14 @@
+﻿namespace MotorReparation.Infrastructure
+{
+    public static class StringDefinition
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+        public const string StatusPending = "Pending";
+        public const string StatusInProcess = "Processing";
+        public const string StatusDone = "Done";
+
+    }
+}
